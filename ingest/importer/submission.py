17421fa87e6c66bcd0a45f73c2b419a40cac541f
Alegria Aclan <aaclan@ebi.ac.uk>
2019-01-31 17:27:51 +0000

    Fix frequency of updating the actual links count in the submission manifest

--- ingest/importer/submission.py
+++ ingest/importer/submission.py
@@ -47,12 +47,10 @@ class IngestSubmitter(object):
                 try:
                     submission.link_entity(entity, to_entity, relationship=link['relationship'])
                     progress = progress + 1
-                    self.logger.info(f'links progress: {progress}')
-                    self.logger.info(f"{submission.manifest['expectedLinks']}")
-                    if progress % self.PROGRESS_CTR or (progress == int(submission.manifest['expectedLinks'])):
+                    if progress % self.PROGRESS_CTR == 0 or (progress == int(submission.manifest['expectedLinks'])):
                         manifest_url = self.ingest_api.get_link_from_resource(submission.manifest, 'self')
                         self.ingest_api.patch(manifest_url, {'actualLinks': progress})
-                        self.logger.info(f'links progress: {progress}')
+                        self.logger.info(f"links progress: {progress}/ {submission.manifest['expectedLinks']}")
 
                 except Exception as link_error:
                     error_message = f'''The {entity.type} with id {entity.id} could not be 
