0576a8b209399734f5a0bdd0dd72bf2c74e99675
Alegria Aclan <aaclan@ebi.ac.uk>
2019-06-09 12:37:43 +0100

    Use new search endpoints to query bundle manifests

--- list_bundles.py
+++ list_bundles.py
@@ -1,71 +1,48 @@
 #!/usr/bin/env python
 
 """
-python list_bundles.py --project-uuid=b6dc9b93-929a-45d0-beb2-5cf8e64872fe --env=prod
+python list_bundles.py prod b6dc9b93-929a-45d0-beb2-5cf8e64872fe
+python list_bundles.py staging 3b41f062-621c-46ca-abad-bce09427934d
 """
 
+import argparse
 import json
-import logging
-import sys
-from optparse import OptionParser
 
 from ingest.api.ingestapi import IngestApi
 
 
-class BundleTracker:
-    def __init__(self, ingest_api):
-        self.ingest_api = ingest_api
+class BundleManifest:
+    def __init__(self, resource):
+        self._object = resource
 
-    def get_bundles(self, project_uuid):
-        project = self.ingest_api.get_project_by_uuid(project_uuid)
-        submissions = self.ingest_api.get_related_entities("submissionEnvelopes", project,
-                                              "submissionEnvelopes")
-        for submission in submissions:
-            bundle_manifests = self.ingest_api.get_related_entities("bundleManifests", submission,
-                                                       "bundleManifests")
-            for bundle_manifest in bundle_manifests:
-                yield bundle_manifest
-
-    iter_rows
-    def generate_bundle_fqids(self, project_uuid):
-        bundle_fqids = []
-        for bundle in self.get_bundles(project_uuid):
-            uuid = bundle.get('bundleUuid')
-            version = bundle.get('bundleVersion')
-            fqid = f'{uuid}{version}'
-            bundle_fqids.append(fqid)
-        return bundle_fqids
+    @property
+    def fqid(self):
+        uuid = self._object.get('bundleUuid')
+        version = self._object.get('bundleVersion')
+        return f'{uuid}.{version}'
 
 
 if __name__ == '__main__':
-    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(format=format, stream=sys.stdout, level=logging.INFO)
+    parser = argparse.ArgumentParser(description='Generates a bundle fqid list given a project uuid')
+    parser.add_argument('env', choices=['dev', 'integration', 'staging', 'prod'], help='environment')
+    parser.add_argument('project_uuid', metavar='project-uuid', type=str, help='Project uuid')
+    parser.add_argument('--filename', type=str, help='Output filename')
+    args = parser.parse_args()
 
-    parser = OptionParser()
-    parser.add_option("-p", "--project-uuid", help="Project UUID")
-    parser.add_option("-e", "--env", help="dev|integration|staging|prod")
-    parser.add_option("-o", "--output-file", help="Output filename")
-    (options, args) = parser.parse_args()
+    project_uuid = args.project_uuid
+    filename = args.filename or f'{args.project_uuid}.json'
+    env = args.env
 
-    if not options.project_uuid:
-        print("Please supply a project-uuid and env")
-        exit(2)
-
-    if not options.env:
-        print("Please supply which environment this script should run")
-        exit(2)
-
-    filename = options.output_file or f'{options.project_uuid}.json'
-
-    infix = f'.{options.env}' if options.env != 'prod' else ''
+    infix = f'.{env}' if env != 'prod' else ''
     url = f'https://api.ingest{infix}.data.humancellatlas.org'
     ingest_api = IngestApi(url)
-    bundle_tracker = BundleTracker(ingest_api=ingest_api)
+    project = ingest_api.get_project_by_uuid(project_uuid)
+    bundle_manifests = ingest_api.get_related_entities("bundles", project, "bundleManifests")
 
-    fqids = bundle_tracker.generate_bundle_fqids(options.project_uuid)
+    bundle_fqids = [BundleManifest(obj).fqid for obj in bundle_manifests]
 
     with open(filename, 'w') as outfile:
-        json.dump(fqids, outfile, indent=4)
+        json.dump(bundle_fqids, outfile, indent=4)
 
-    print(f'Total bundle count: {len(fqids)}')
+    print(f'Total bundle count: {len(bundle_fqids)}')
     print(f'Saved into file: {filename}')
